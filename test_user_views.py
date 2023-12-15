82328adfc29f5401093785316b6fe8e859caffcc
Charles Chitty <charlescchitty@gmail.com>
2023-12-15 11:09:56 -0800

    More user test routes added

--- test_user_views.py
+++ test_user_views.py
@@ -6,6 +6,7 @@
 
 
 import os
+from flask import session
 from unittest import TestCase
 
 from models import db, Message, User
@@ -43,8 +44,10 @@ class UserBaseViewTestCase(TestCase):
     def setUp(self):
         User.query.delete()
 
-        self.u1 = User.signup("u1", "u1@email.com", "password", None)
-        self.u2 = User.signup("u2", "u2@email.com", "password", None)
+        self.valid_password = "password"
+
+        self.u1 = User.signup("u1", "u1@email.com", self.valid_password, None)
+        self.u2 = User.signup("u2", "u2@email.com", self.valid_password, None)
 
         db.session.commit()
 
@@ -64,7 +67,7 @@ class UserAddViewTestCase(UserBaseViewTestCase):
                 sess[CURR_USER_KEY] = self.u1.id
 
             resp = c.get('/')
-            html = resp.text
+            html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Comment for home.html loaded.', html)
@@ -74,7 +77,7 @@ class UserAddViewTestCase(UserBaseViewTestCase):
 
         with app.test_client() as c:
             resp = c.get('/')
-            html = resp.text
+            html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Comment for home-anon.html loaded.', html)
@@ -84,10 +87,23 @@ class UserAddViewTestCase(UserBaseViewTestCase):
 
         with app.test_client() as c:
             resp = c.get("/signup")
-            html = resp.text
+            html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Comment for users/signup.html loaded.', html)
+
+    def test_signup_form_logged_in(self):
+        """Should log you out if you load signup form while logged in"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+            resp = c.get("/signup")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Comment for users/signup.html loaded.', html)
+            self.assertEqual(session.get(CURR_USER_KEY), None)
 
     def test_signup_submit_ok(self):
         """Should sign up user and redirect to home"""
@@ -125,9 +141,142 @@ class UserAddViewTestCase(UserBaseViewTestCase):
                     },
                 follow_redirects=True,
             )
-            html = resp.text
+            html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Comment for users/signup.html loaded.', html)
             self.assertIn("Username or email already taken", html)
 
+    def test_login_form(self):
+        """Should load login form"""
+
+        with app.test_client() as c:
+            resp = c.get("/login")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Comment for users/login.html loaded.', html)
+
+    def test_login_ok(self):
+        """Should sign up user and redirect to home"""
+
+        with app.test_client() as client:
+            resp = client.post(
+                "/login",
+                data={
+                    "username": self.u1.username,
+                    "password": self.valid_password,
+                    }
+            )
+            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(resp.location, "/")
+            self.assertEqual(session.get(CURR_USER_KEY), self.u1.id)
+
+    def test_login_bad(self):
+        """Should redirect back to form if username or email taken"""
+
+        with app.test_client() as client:
+            resp = client.post(
+                "/login",
+                data={
+                    "username": self.u1.username,
+                    "password": "wrong-wrong",
+                }
+            )
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Invalid credentials", html)
+            self.assertEqual(session.get(CURR_USER_KEY), None)
+
+    def test_logout(self):
+        """Should log out user and redirect to homepage"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+            resp = c.post(
+                "/logout"
+            )
+            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(resp.location, "/")
+            self.assertEqual(session.get(CURR_USER_KEY), None)
+
+    def test_list_users(self):
+        """Should list warbler users if logged-in"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+            resp = c.get('/users')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Comment for users/index.html loaded.", html)
+            self.assertEqual(session.get(CURR_USER_KEY), self.u1.id)
+
+    #Test searching users
+
+
+
+    def test_show_user(self):
+        """Should show data about a user if logged in"""
+
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+            resp = c.get(
+                f"/users/{self.u1.id}",
+            )
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("Comment for users/show.html loaded.", html)
+            self.assertIn(self.u1.username, html)
+
+    # def test_show_user_unauth(self):
+    #     with app.test_client() as client:
+    #         with client.session_transaction() as sess:
+    #             sess["username"] = "wrong"
+    #         resp = client.get(
+    #             "/users/user-1",
+    #         )
+    #         self.assertEqual(resp.status_code, 401)
+
+    # def test_show_user_404(self):
+    #     with app.test_client() as client:
+    #         with client.session_transaction() as sess:
+    #             sess["username"] = "wrong"
+    #         resp = client.get(
+    #             "/users/wrong",
+    #         )
+    #         self.assertEqual(resp.status_code, 404)
+
+    # def test_remove_user(self):
+    #     with app.test_client() as client:
+    #         with client.session_transaction() as sess:
+    #             sess["username"] = "user-1"
+    #         resp = client.post(
+    #             "/users/user-1/delete",
+    #         )
+    #         self.assertEqual(resp.status_code, 302)
+    #         self.assertEqual(resp.location, "/login")
+    #         self.assertEqual(session.get(AUTH_KEY), None)
+
+    # def test_remove_user_unauth(self):
+    #     with app.test_client() as client:
+    #         with client.session_transaction() as sess:
+    #             sess["username"] = "wrong"
+    #         resp = client.post(
+    #             "/users/user-1/delete",
+    #         )
+    #         self.assertEqual(resp.status_code, 401)
+
+    # def test_remove_user_404(self):
+    #     with app.test_client() as client:
+    #         with client.session_transaction() as sess:
+    #             sess["username"] = "wrong"
+    #         resp = client.post(
+    #             "/users/wrong/delete",
+    #         )
+    #         self.assertEqual(resp.status_code, 404)
